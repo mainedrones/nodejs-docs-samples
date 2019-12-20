47d8e88d691cbaed228f3ca7a81256b056b929c1
737leap <56855173+737leap@users.noreply.github.com>
2019-12-20 18:35:07 -0500

    Update index.js

--- functions/helloworld/index.js
+++ functions/helloworld/index.js
@@ -31,7 +31,7 @@ const escapeHtml = require('escape-html');
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.helloGET = (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Planet Earth!');
 };
 // [END functions_helloworld_get]
 
